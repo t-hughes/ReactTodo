026972c292d54760fe82151206bd2376d830e90b
Talon Hughes <talon.hughes@gmail.com>
2017-05-03 16:01:24 -0500

    added playground to practice spread operator

---
+++ playground/spread.js
@@ -0,0 +1,35 @@
+// function add (a, b) {
+//   return a + b;
+// }
+//
+// console.log(add(3,1)); //4
+//
+// var toAdd = [9, 5];
+// console.log(add(...toAdd)); //14
+//
+
+// Array Spread Operator
+// var groupA = ['Lauren', 'Cohen'];
+// var groupB = ['Auden'];
+//
+// var final = [...groupB, 3, ...groupA];
+//
+// console.log(final);
+
+// Challenge 1 - create function that greets the person and displays their age
+// var person = ['Talon', 27];
+// var personTwo = ['Lauren', 23];
+//
+// function greet (name, age) {
+//   console.log('Hi ' + name + ', you are ' + age);
+// }
+// greet(...person);
+// greet(...personTwo);
+
+// Challenge 2 - create function that combines both arrays and then loopsover the final array and prints out a greeting.
+// var names = ['Bob', 'Joe'];
+// var final = ['Talon', ...names];
+//
+// final.forEach(function (name) {
+//   console.log('Hi ' + name);
+// });
