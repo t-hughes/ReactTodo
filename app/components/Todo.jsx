0525f45e06dedb348e7a2ff756b8510860d80f38
Talon Hughes <talon.hughes@gmail.com>
2017-05-05 15:13:25 -0500

    added completed and created timestamps using moment.js and accompanying tests

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,15 +1,28 @@
-var React = require("react");
+var React = require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
-  render: function() {
-    var { id, text, completed } = this.props;
+  render: function () {
+    var { id, text, completed, createdAt, completedAt} = this.props;
+    var renderDate = () => {
+      var message = 'Created ';
+      var timeStamp = createdAt;
+
+      if (completed) {
+        message = 'Completed ';
+        timeStamp = completedAt;
+      }
+
+      return message + moment.unix(timeStamp).format('MMM Do, YYYY @ h:mm A');
+    };
+    
     return (
-      <div
-        onClick={() => {
-          this.props.onToggle(id);
-        }}
-      >
-        <input type="checkbox" checked={completed} /> {text}
+      <div onClick={() => {
+        this.props.onToggle(id);
+      }}>
+        <input type="checkbox" checked={completed}/> 
+        <p>{text}</p>
+        <p>{renderDate()}</p>
       </div>
     );
   }
---
+++ playground/moment-examples.js
@@ -0,0 +1,17 @@
+var moment = require('moment');
+
+console.log(moment().format());
+
+// January 1st 1970 @ 12:00am -> 0
+// January 1st 1970 @ 12:00am -> 60
+
+var now = moment();
+
+console.log('Current timestamp', now.unix());
+
+var timeStamp = 1494012748;
+var currentMoment = moment.unix(timeStamp);
+console.log('current moment', currentMoment.format('MMM D, YYYY @ h:mm A'));
+
+// January 3rd, 2017 @ 12:13 AM
+console.log('current moment', currentMoment.format('MMMM Do, YYYY @ h:mm A'));
