99dcc7f2de22ac88c37a5b87cddcc8fd87e8cd0f
Talon Hughes <talon.hughes@gmail.com>
2017-05-12 14:52:11 -0600

    moved firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyBlIgYSvK9CnDC2F_DR-wB2l-iphkflULc",
-    authDomain: "alfred-todo-app.firebaseapp.com",
-    databaseURL: "https://alfred-todo-app.firebaseio.com",
-    projectId: "alfred-todo-app",
-    storageBucket: "alfred-todo-app.appspot.com",
-    messagingSenderId: "1038267635039"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
-
+  
   firebase.initializeApp(config);
 } catch (e) {
 
